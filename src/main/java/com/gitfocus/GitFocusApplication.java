9a40f4b3b579f99b4eb42b7f5282f6ee8f54a3d0
Senthilkumar <psysenthilkumar@gmail.com>
2020-05-10 17:40:32 +0530

    comment msg

--- src/main/java/com/gitfocus/GitFocusApplication.java
+++ src/main/java/com/gitfocus/GitFocusApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitFocusApplication {
-// comment
+// comment main
 	public static void main(String[] args) {
 		SpringApplication.run(GitFocusApplication.class, args);
 	}
