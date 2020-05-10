7e012616980b6db66e6013500c95cbb302d15852
Senthilkumar <psysenthilkumar@gmail.com>
2020-05-10 17:37:04 +0530

    Initial Commit

---
+++ src/main/java/com/gitfocus/GitFocusApplication.java
@@ -0,0 +1,13 @@
+package com.gitfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFocusApplication.class, args);
+	}
+
+}
