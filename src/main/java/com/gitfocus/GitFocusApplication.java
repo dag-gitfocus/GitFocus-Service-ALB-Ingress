d81a44e6f8263b2209846b809106e401dbfad6d1
Senthilkumar <psysenthilkumar@gmail.com>
2020-05-11 11:06:44 +0530

    Property file changes

--- src/main/java/com/gitfocus/GitFocusApplication.java
+++ src/main/java/com/gitfocus/GitFocusApplication.java
@@ -1,13 +1,22 @@
 package com.gitfocus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Tech Mahindra
+ * Initial class to boot GitFocus Application
+ */
 @SpringBootApplication
 public class GitFocusApplication {
-// comment main
-	public static void main(String[] args) {
-		SpringApplication.run(GitFocusApplication.class, args);
-	}
+
+    private static final Logger logger = LoggerFactory.getLogger(GitFocusApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting GitFocus-Service Application..");
+        SpringApplication.run(GitFocusApplication.class, args);
+    }
 
 }
