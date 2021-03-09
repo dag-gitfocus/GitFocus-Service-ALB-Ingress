f6c8515d9cef382d71d01c3f5439dd54d7beb3be
Senthilkumar <psysenthilkumar@gmail.com>
2021-03-09 16:41:37 +0530

    mIN

--- src/main/java/com/gitfocus/GitFocusApplication.java
+++ src/main/java/com/gitfocus/GitFocusApplication.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Tech Mahindra 
@@ -19,6 +21,17 @@ public class GitFocusApplication {
 		// TODO Auto-generated constructor stub
 		logger.info("Starting GitFocus-Service Application..");
 	}
+
+	@RestController
+	class HelloSpring {
+
+		@GetMapping("/hellospring")
+		public String helloSpringProfile() {
+
+			return "Hello Spring ....!";
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitFocusApplication.class, args);
 	}
